467a20013e24a81aa892d60a4c9862d40c805abc
will <314112362@qq.com>
2017-07-28 12:59:32 +0800

    update: 阿里支付

--- application/api/controller/Pay.php
+++ application/api/controller/Pay.php
@@ -8,26 +8,28 @@
 
 namespace app\api\controller;
 
-use pay\alipay_mobile;
 use EasyWeChat\Foundation\Application;
 use EasyWeChat\Payment\Order;
-
-use Payment\Common\PayException;
 use Payment\Client\Charge;
+use Payment\Common\PayException;
 use Payment\Config;
-class Pay extends BaseController {
+
+class Pay extends BaseController{
 
     /**
-     * @api {GET} /pay 我的钱包done
-     * @apiName index
+     * @api      {GET} /pay 我的钱包done
+     * @apiName  index
      * @apiGroup Pay
      * @apiHeader {String} authorization-token      token.
      * @apiSuccess {String} balance         账户余额
      * @apiSuccess {String} bonus         我的推荐奖励
      */
-    public function index() {
+    public function index(){
         $spBaseInfo = model('SpBaseInfo', 'logic')->findInfoByUserId($this->loginUser['id']);
-        returnJson('2000', '成功', ['balance' => wztxMoney($spBaseInfo['balance']), 'bonus' => wztxMoney($spBaseInfo['bonus'])]);
+        returnJson('2000', '成功', [
+            'balance' => wztxMoney($spBaseInfo['balance']),
+            'bonus' => wztxMoney($spBaseInfo['bonus'])
+        ]);
     }
 
     /*
@@ -40,39 +42,40 @@ class Pay extends BaseController {
      * @apiSuccess {String}     account               收款账号
      * @apiSuccess {String}     real_name            开户名称
      */
-    public function payBond() {
+    public function payBond(){
         //判断是否缴纳保证金->是否个人用户得到保证金金额 对比-》缴纳保证金
         $status = bondStatus($this->loginUser['id']);
         if($status == 'checked'){
-            returnJson(4000,'您已缴纳过保证金');
+            returnJson(4000, '您已缴纳过保证金');
         }
         if($this->loginUser['type'] == 'person'){
             $amount = getSysconf('bond_person_amount');
         }elseif($this->loginUser['type'] == 'company'){
             $amount = getSysconf('bond_company_amount');
         }
-        echo $amount;die;
+        echo $amount;
+        die;
         if(!$ispass){
-            returnJson(4000,'您未认证或未缴纳保证金');
+            returnJson(4000, '您未认证或未缴纳保证金');
         }
     }
 
     /**
-     * @api {POST} /pay/recharge  充值
-     * @apiName recharge
+     * @api      {POST} /pay/recharge  充值
+     * @apiName  recharge
      * @apiGroup Pay
      * @apiHeader {String} authorization-token      token.
      * @apiParam  {String}  real_amount              充值金额
      * @apiParam  {Int}    pay_way                  支付方式 1=支付宝，2=微信
      * @apiSuccess {Array} pay_info                 支付返回信息
      */
-    public function recharge() {
+    public function recharge(){
 
     }
 
     /**
-     * @api {POST}  /pay/showPayRecord 查看账单done
-     * @apiName showPayRecord
+     * @api      {POST}  /pay/showPayRecord 查看账单done
+     * @apiName  showPayRecord
      * @apiGroup Pay
      * @apiHeader {String} authorization-token          token.
      * @apiParam {Number} [page=1]                      页码.
@@ -93,7 +96,7 @@ class Pay extends BaseController {
      * @apiSuccess {Number} dataTotal                   数据总数.
      * @apiSuccess {Number} pageTotal                   总页码数.
      */
-    public function showPayRecord() {
+    public function showPayRecord(){
         $paramAll = $this->getReqParams([
             'is_pay',
         ]);
@@ -107,15 +110,15 @@ class Pay extends BaseController {
         $where['sp_id'] = $this->loginUser['id'];
         $pageParam = $this->getPagingParams();
         $orderInfo = model('TransportOrder', 'logic')->getTransportOrderList($where, $pageParam);
-        if (empty($orderInfo)) {
+        if(empty($orderInfo)){
             returnJson('4004', '暂无订单信息');
         }
         $list = [];
-        foreach ($orderInfo['list'] as $k => $v) {
+        foreach($orderInfo['list'] as $k => $v){
             $list[$k]['order_id'] = $v['id'];
             $list[$k]['org_city'] = $v['org_city'];
             $list[$k]['dest_city'] = $v['dest_city'];
-            if (!empty($v['dr_id'])) {
+            if(!empty($v['dr_id'])){
                 $drBaseInfo = model('DrBaseInfo', 'logic')->findInfoByUserId($v['dr_id']);
                 $dr_real_name = $drBaseInfo['real_name'];
             }else{
@@ -133,8 +136,8 @@ class Pay extends BaseController {
     }
 
     /**
-     * @api {POST} /pay/rechargeRecord  充值记录done
-     * @apiName rechargeRecord
+     * @api      {POST} /pay/rechargeRecord  充值记录done
+     * @apiName  rechargeRecord
      * @apiGroup Pay
      * @apiHeader {String} authorization-token      token.
      * @apiParam {Number} [page=1]                  页码.
@@ -149,17 +152,17 @@ class Pay extends BaseController {
      * @apiSuccess {Number} dataTotal           数据总数.
      * @apiSuccess {Number} pageTotal           总页码数.
      */
-    public function rechargeRecord() {
+    public function rechargeRecord(){
         $where['sp_id'] = $this->loginUser['id'];
         $where['type'] = $this->loginUser['type'];
         $pageParam = $this->getPagingParams();
         $recordInfo = model('Pay', 'logic')->getRechargeRecordList($where, $pageParam);
-        if (empty($recordInfo)) {
+        if(empty($recordInfo)){
             returnJson('4004', '暂无订单信息');
         }
         $list = $recordInfo['list'];
 
-        foreach ($list as $k => $v) {
+        foreach($list as $k => $v){
             $list[$k]['real_amount'] = wztxMoney($v['real_amount']);
             $list[$k]['balance'] = wztxMoney($v['balance']);
             $list[$k]['pay_time'] = wztxDate($v['pay_time']);
@@ -169,38 +172,60 @@ class Pay extends BaseController {
     }
 
     /**
-     * @api {POST} /pay/alipay  支付宝支付done
-     * @apiName alipay
+     * @api      {POST} /pay/alipay  支付宝支付done
+     * @apiName  alipay
      * @apiGroup Pay
      * @apiHeader {String} authorization-token      token.
      * @apiParam  {Number}  order_id                订单ID
      * @apiParam  {Number}  total_amount            支付金额
      */
     public function alipay(){
+        //	public $partner_public_key  = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCitD16CypwZILTpdJL8nPV9rVFHYf5UWa/URNX6469mbQLpWfjKM/VSWRXsNVGSM3itOO/KG2Pw4x5g9xjH6iaE4LlaidjBIPpifISSlnpbyi4HxQTZYgMPv/TuiWofUN5kcwg/KQAQxB2OwTOeFu2i3LhqSCDmv6koTvHW15/hQIDAQAB";
+        $partner_public_key = getenv('ALIPAY_PARTNER_PUBLIC_KEY');
+        //	public $alipay_public_key   = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDIgHnOn7LLILlKETd6BFRJ0GqgS2Y3mn1wMQmyh9zEyWlz5p1zrahRahbXAfCfSqshSNfqOmAQzSHRVjCqjsAw1jyqrXaPdKBmr90DIpIxmIyKXv4GGAkPyJ/6FTFY99uhpiq0qadD/uSzQsefWo0aTvP/65zi3eof7TcZ32oWpwIDAQAB";
+        $alipay_public_key = getenv('ALIPAY_ALI_PUBLIC_KEY');
+        //公用变量
+        $serverUrl = 'http://publicexprod.d5336aqcn.alipay.net/chat/multimedia.do';//'http://publicexprod.d5336aqcn.alipay.net/chat/multimedia.do';//'http://i.com/works/photo-sdk/_data/1.jpg';//"http://i.com/works/photo-sdk/_data/publicexprod.php";//"http://publicexprod.d5336aqcn.alipay.net/chat/multimedia.do";
+        //	public $appId = "2013121100055554";
+        $appId = getenv("ALIPAY_APPID");
+
+        //	public $partner_private_key = 'MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAKK0PXoLKnBkgtOl0kvyc9X2tUUdh/lRZr9RE1frjr2ZtAulZ+Moz9VJZFew1UZIzeK0478obY/DjHmD3GMfqJoTguVqJ2MEg+mJ8hJKWelvKLgfFBNliAw+/9O6Jah9Q3mRzCD8pABDEHY7BM54W7aLcuGpIIOa/qShO8dbXn+FAgMBAAECgYA8+nQ380taiDEIBZPFZv7G6AmT97doV3u8pDQttVjv8lUqMDm5RyhtdW4n91xXVR3ko4rfr9UwFkflmufUNp9HU9bHIVQS+HWLsPv9GypdTSNNp+nDn4JExUtAakJxZmGhCu/WjHIUzCoBCn6viernVC2L37NL1N4zrR73lSCk2QJBAPb/UOmtSx+PnA/mimqnFMMP3SX6cQmnynz9+63JlLjXD8rowRD2Z03U41Qfy+RED3yANZXCrE1V6vghYVmASYsCQQCoomZpeNxAKuUJZp+VaWi4WQeMW1KCK3aljaKLMZ57yb5Bsu+P3odyBk1AvYIPvdajAJiiikRdIDmi58dqfN0vAkEAjFX8LwjbCg+aaB5gvsA3t6ynxhBJcWb4UZQtD0zdRzhKLMuaBn05rKssjnuSaRuSgPaHe5OkOjx6yIiOuz98iQJAXIDpSMYhm5lsFiITPDScWzOLLnUR55HL/biaB1zqoODj2so7G2JoTiYiznamF9h9GuFC2TablbINq80U2NcxxQJBAMhw06Ha/U7qTjtAmr2qAuWSWvHU4ANu2h0RxYlKTpmWgO0f47jCOQhdC3T/RK7f38c7q8uPyi35eZ7S1e/PznY=';
+        $partner_private_key = getenv('ALIPAY_PARTNER_PRIVATE_KEY');
+
         //require_once APP_PATH.'/alipay/AopSdk.php';
         include(APP_PATH.'/alipay/AopSdk.php');
         $aop = new \AopClient();
         $aop->gatewayUrl = "https://openapi.alipay.com/gateway.do";
-        $aop->appId = "2017061607503256";
-        $aop->rsaPrivateKey = 'MIIEpAIBAAKCAQEA8vg4xtApZfufZ/rSXaRE6PdZehfcTH4/ks5dR+FberJ6/62BvlHSlubGruPA7jLc7GdAd2nnbAXU7sGqRXilvbkjAdYnTxbwXjY+sazKGTSgEzNgR5Vpek5fMpMudMjOjiTTuasIOH8r9pBXSh5Fg4ear1esKa/em82TmxnkP7Lf2nJZKS6OIfs5Ory5KKRJE5oh+HFvgLDq6JWXhejUFWlLJ/snBetJdMFolP7OJ47PlIBvvAPXhrXhuh/3PIjwZu5gCqeTbJ7wBbxEhRrCqLEGjLKc15e0YNSuljkjCNQ0ErQG2DjdNLCkaJq76fJXoF72fdg04TP0eTNyVZpGHQIDAQABAoIBAF8nOwURjMT10C3mmvA1Xw9ln1MjeREz+C3ER9/Yr/zTXTw4dTFV1gVnB7SCWZJvtPmYTjT18r3pYsTGb6qZXz93++/CMM7Wivg6gj8PDm7knzQl0LT4HMDbZIjn/y+ZXNtqLMjv5F5L36nGSYkrZcnnF3tH+JKy35lg30fE0hDndz7WlGxu1OXBZ7vVVmRQX2ia3bmG2LuiV9ryt98hdPmlhkDUVB8jMsxOoQwWhSR4CdATum+SLZOHSHVY21l1mmUuRnEmK+H77CqylkFeUPzxUCKANZvlT6pb4csF0WEa51OP4YjJZK7DF2B1v3ewkbYtJ/ZuazIvT+fHqGvPtDkCgYEA/9QknJdprf+yREjYBs+sKogajNi/rpilvoKuL2hCyzq1FYEAmlgxEeJcDYcmnux6yMM6LxTF1IWCv53JLNl3tFzhrrwGOsRa7byP2yE5CMQYuGpU7INC30xW1ohGQnvgRFULIULtHrzeG+7hY5yNyhQBAmO4OX3Ssv2lB75KPHcCgYEA8yHf08ylC1YwKnRdwyXQPrE352188nQxCnj9Lq91mTDvxuoKPVQa8sYwKMbKvzCS0SJ9WGtFTS0oZqrrFM/beX40Eyke7qYnLoaMNxSPfpz6AKfl9NTuPcDkQibjabpLDC7Q4fxcYuwqKnFBLOt3AuX3osu5JpxZE3NNTV8n+wsCgYEAq2TynmKmr6cmRK9U48NQgjIrL3+rdArayDb/Ac3lKgkL9vs1bzJ0tZmkuH96dXDTlhuNqKtPGuHTxhKtDDoqA5FSteFMfyS8EpiI/HNWpbPTKAI9ITOTosyfRR2JjNM3XjBnw4H2IOjCGY7CPB1PtToPrw0mCIZumfJrFTP8wmMCgYEA7E1wDZpIjswl5B1VQ+XskAIOI4/2cG8deuA8srM1yL4XTW0KprCnwG1/QSJ0y32aNEkhKl6X7HqHWcGk2YVr+pj+Y+EDf09dpYp/nMkO7jADi7+jcGHDa6GeN+0z+f5mEmEuA3YTFNIT6UxJ3C6+bMK1/DOksDIlIRJff2OMqCECgYBm6IYTNYBsjmJO2M41o/PbPkjIlepLN3JZrLn9Nx7x1JdUuHjfUm3lJYNBXbS8wrg2Oi8Mp4nxh+l+A9pFYKDnbQ862F18ZUYdGklBV8fkSKJ/pvlxIheKVEt1iYrvYo0m5R7TgDeefDzXjzhHtiS6fegbbcEX4avRMrT27fukEg==' ;
+        $aop->appId = $appId;
+        $aop->rsaPrivateKey = $partner_private_key;
         $aop->format = "json";
-        $aop->charset = "UTF-8";
+        $aop->charset = "utf-8";
         $aop->signType = "RSA2";
         $aop->debugInfo = true;
-        $aop->alipayrsaPublicKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAx57k5O9rmVm3WyYXuL5JVQ9PhyGe7ih3AJL/FySC9fas6InkoBtoo+kXsZWTn9rxhOFkMl0mmL7TSDZCWZLX6KNs5cmZh+6yTm++LhPaFsYZnwOsaZlmD6P57g9rJhvfY0LPKZ4I5qdMUQ9uYsaNp4vf7rg9VS1d1KS9I9Qpen0WYsp3zPM/B+ivQk6GpLaR3Q+60dusLc/P7STiIUl8GlfK9efE5h0Ajs5NVES+MmY2Ez1ouoTbnjiZ5ls110zBHLSubeEEB6IIAbsJJdG5L6EiimNsXrTm73QJO0ij31kkb7BsNisb8SuXcdlbdYklfVdY9qT7PshfS8FglxbC5wIDAQAB';
+        $aop->alipayrsaPublicKey = $alipay_public_key;
         //实例化具体API对应的request类,类名称和接口名称对应,当前调用接口名称：alipay.trade.app.pay
         $request = new \AlipayTradeAppPayRequest();
         //SDK已经封装掉了公共参数，这里只需要传入业务参数
-        $bizcontent = "{'body':'我是测试数据','subject': 'App支付测试','out_trade_no': '20170125test01','timeout_express': '30m','total_amount': '0.01','product_code':'QUICK_MSECURITY_PAY'}";
+        $bizData =[
+            'body'=>'我是测试数据',
+            "subject"=>"App支付测试",
+            "out_trade_no"=>"20170125test01",
+            "timeout_express"=>"30m",
+            "total_amount"=>"1",
+            "product_code"=>"QUICK_MSECURITY_PAY",
+        ];
+
+        $bizcontent = json_encode($bizData);
         $request->setNotifyUrl("http://wztx.shp.api.ruitukeji.com/Callback/alipay_callback");
         $request->setBizContent($bizcontent);
         //这里和普通的接口调用不同，使用的是sdkExecute
         $response = $aop->sdkExecute($request);
         //returnJson(2000,'成功',$response);die;
         //htmlspecialchars是为了输出到页面时防止被浏览器将关键参数html转义，实际打印到日志以及http传输不会有这个问题
+        $orderString = htmlspecialchars($response);//就是orderString 可以直接给客户端请求，无需再做处理。
 
-        $str =  htmlspecialchars($response);//就是orderString 可以直接给客户端请求，无需再做处理。
-        returnJson(2000,'成功',$str);
+        returnJson(2000, '成功', $orderString);
+
         die;
         /*$aliConfig = require_once APP_PATH.'aliconfig.php';
 
@@ -266,8 +291,8 @@ class Pay extends BaseController {
     }
 
     /**
-     * @api {POST} /pay/wxpay  微信支付done
-     * @apiName wxpay
+     * @api      {POST} /pay/wxpay  微信支付done
+     * @apiName  wxpay
      * @apiGroup Pay
      * @apiHeader {String} authorization-token      token.
      * @apiParam  {Number}  order_id                订单ID
@@ -275,38 +300,38 @@ class Pay extends BaseController {
      */
     public function wxpay(){
         $wxConfig = require_once APP_PATH.'wxconfig.php';
-        $orderNo = time() . rand(1000, 9999);
+        $orderNo = time().rand(1000, 9999);
         // 订单信息
         $payData = [
-            'body'    => 'test body',
-            'subject'    => 'test subject',
-            'order_no'    => $orderNo,
+            'body' => 'test body',
+            'subject' => 'test subject',
+            'order_no' => $orderNo,
             'timeout_express' => time() + 600,// 表示必须 600s 内付款
-            'amount'    => '3.01',// 微信沙箱模式，需要金额固定为3.01
+            'amount' => '3.01',// 微信沙箱模式，需要金额固定为3.01
             'return_param' => '123',
             'client_ip' => isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '127.0.0.1',// 客户地址
         ];
 
-        try {
+        try{
             $ret = Charge::run(Config::WX_CHANNEL_APP, $wxConfig, $payData);
-        } catch (PayException $e) {
+        }catch(PayException $e){
             echo $e->errorMessage();
             exit;
         }
 
         //$str = json_encode($ret, JSON_UNESCAPED_UNICODE);
-        returnJson(2000,'成功',$ret);
+        returnJson(2000, '成功', $ret);
         die;
 
         $ispass = ispassAuth($this->loginUser);
         if(!$ispass){
-            returnJson(4000,'您未认证或未缴纳保证金');
+            returnJson(4000, '您未认证或未缴纳保证金');
         }
         $paramAll = $this->getReqParams(['order_id']);
         $rule = [
             'order_id' => 'require'
         ];
-        validateData($paramAll,$rule);
+        validateData($paramAll, $rule);
         $options = [
             // 前面的appid什么的也得保留哦
             //'app_id' => 'wx6470b69abdf65e06',
@@ -314,25 +339,25 @@ class Pay extends BaseController {
             // payment
             'payment' => [
                 //'merchant_id'        => '1383659202',
-                'merchant_id'        => '1483170282',
+                'merchant_id' => '1483170282',
                 //'key'                => '2D2C5B0CDFA135D8FAB37227B0F569E5',
-                'key'                => 'd39be96f2e538480fc567ea12d54c59e',//RUITU111111KEJImd5
-                'cert_path'          => '/wxpayment/apiclient_cert.pem', // XXX: 绝对路径！！！！
-                'key_path'           => '/wxpayment/apiclient_key.pem',      // XXX: 绝对路径！！！！
-                'notify_url'         => 'http://wztx.shp.api.ruitukeji.com/callback/wxpay_callback',       // 你也可以在下单时单独设置来想覆盖它
+                'key' => 'd39be96f2e538480fc567ea12d54c59e',//RUITU111111KEJImd5
+                'cert_path' => '/wxpayment/apiclient_cert.pem', // XXX: 绝对路径！！！！
+                'key_path' => '/wxpayment/apiclient_key.pem',      // XXX: 绝对路径！！！！
+                'notify_url' => 'http://wztx.shp.api.ruitukeji.com/callback/wxpay_callback',       // 你也可以在下单时单独设置来想覆盖它
             ],
         ];
         $app = new Application($options);
         $payment = $app->payment;
 
         $attributes = [
-            'trade_type'       => 'APP', // JSAPI，NATIVE，APP...
-            'body'             => 'biaoti',//标题
-            'detail'           => 'xiangxijieshao',//详细介绍
-            'out_trade_no'     => '2017072310254561',//商家订单号
-            'total_fee'        => 1,
+            'trade_type' => 'APP', // JSAPI，NATIVE，APP...
+            'body' => 'biaoti',//标题
+            'detail' => 'xiangxijieshao',//详细介绍
+            'out_trade_no' => '2017072310254561',//商家订单号
+            'total_fee' => 1,
             // 'total_fee'        => $order_info['meal_price'],
-            'notify_url'       => 'http://wztx.shp.api.ruitukeji.com/callback/wxpay_callback', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
+            'notify_url' => 'http://wztx.shp.api.ruitukeji.com/callback/wxpay_callback', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
             // ...
         ];
 
@@ -340,33 +365,32 @@ class Pay extends BaseController {
 
         $result = $payment->prepare($order);
         //dump($result);
-        if ($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS'){
+        if($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS'){
             $prepayId = $result->prepay_id;
         }else{
-            returnJson(4000,'调用支付失败');
+            returnJson(4000, '调用支付失败');
         }
 
-        $app_data['appid']=$options['app_id'];
-        $app_data['partnerid']=$options['payment']['merchant_id'];
-        $app_data['prepayid']=$prepayId;
-        $app_data['package']='Sign=WXPay';
-        $app_data['noncestr']=uniqid();
-        $app_data['timestamp']=time();
+        $app_data['appid'] = $options['app_id'];
+        $app_data['partnerid'] = $options['payment']['merchant_id'];
+        $app_data['prepayid'] = $prepayId;
+        $app_data['package'] = 'Sign=WXPay';
+        $app_data['noncestr'] = uniqid();
+        $app_data['timestamp'] = time();
 
         $params = array_filter($app_data);
         $params['sign'] = $this->generate_sign($params, $options['payment']['key'], 'md5');
-        returnJson(2000,'成功',$params);
+        returnJson(2000, '成功', $params);
     }
 
     /**
      * 生成微信sign
-     * @param array $attributes
-     * @param $key
+     * @param array  $attributes
+     * @param        $key
      * @param string $encryptMethod
      * @return string
      */
-    public function generate_sign(array $attributes, $key, $encryptMethod = 'md5')
-    {
+    public function generate_sign(array $attributes, $key, $encryptMethod = 'md5'){
         ksort($attributes);
 
         $attributes['key'] = $key;
