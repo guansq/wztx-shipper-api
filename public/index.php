698ca4009c0121f1cc86ac2c9133943d3a99b2a5
guanshaoqiu <94600115@qq.com>
2018-01-24 10:56:29 +0800

    '修复兼容phpstudy'

--- public/index.php
+++ public/index.php
@@ -16,4 +16,5 @@ define('APP_PATH', __DIR__ . '/../application/');
 define('ENV_PREFIX', ''); // 环境变量的配置前缀
 // 加载框架引导文件
 $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI' ];
+define('BIND_MODULE','api');
 require __DIR__ . '/../thinkphp/start.php';
